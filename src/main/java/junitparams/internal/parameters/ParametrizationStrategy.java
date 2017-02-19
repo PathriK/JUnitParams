f5907a45c6248b94411598b9927e894558ca564e
pathrik <pathrikumark@gmail.com>
2017-02-19 16:41:50 +0530

    More slimming down
    Deleted previously commented files

--- src/main/java/junitparams/internal/parameters/ParametrizationStrategy.java
+++
@@ -1,7 +0,0 @@
-//TODO: Remove file
-//package junitparams.internal.parameters;
-//
-//interface ParametrizationStrategy {
-//    Object[] getParameters();
-//    boolean isApplicable();
-//}
--- src/main/java/junitparams/naming/TestCaseNamingStrategy.java
+++
@@ -1,9 +0,0 @@
-//TODO: Remove file
-//package junitparams.naming;
-//
-///**
-// * A strategy that can resolve a test case method name by it's parameters.
-// */
-//public interface TestCaseNamingStrategy {
-//    String getTestCaseName(int parametersIndex, Object parameters);
-//}
